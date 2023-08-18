feab6c97c8d609eb6f3f41116a95c256e4867531
Jonas Sasin <xsasin1@fi.muni.cz>
2023-08-18 17:24:40 +0200

    Search by opportunity api call

---
+++ src/web-app/src/components/display/OpportunitySearchResults.tsx
@@ -0,0 +1,41 @@
+import { FC } from 'react';
+import { Box, Grid, Typography } from '@mui/material';
+
+import ColoredLine from './ColoredLine';
+import { OpportunitySearchResultDto } from '../../types/Search.Types';
+
+type Props = {
+	searchResult: OpportunitySearchResultDto[];
+};
+
+/**
+ * Search results display component
+ * @param searchResult Search results (opportunities) to display
+ * @constructor
+ */
+const OpportunitySearchResults: FC<Props> = ({ searchResult }) => (
+	<>
+		<Box height={10} id="search-results-reference" />
+		<Grid container>
+			<Typography variant="h5">Results:</Typography>
+			<Grid item xs={12}>
+				<ColoredLine color="grey" height={2} />
+			</Grid>
+			{searchResult && searchResult.length === 0 && (
+				<Typography>No results, try different filtering please.</Typography>
+			)}
+			{searchResult && searchResult.length > 0 && (
+				<Typography>Showing {searchResult.length} results</Typography>
+			)}
+		</Grid>
+		<Grid container justifyContent="center" rowSpacing={3} columnSpacing={1}>
+			{searchResult.map(result => (
+				<Grid item container key={result.author.uco}>
+					<Grid item xs={11}></Grid>
+				</Grid>
+			))}
+		</Grid>
+	</>
+);
+
+export default OpportunitySearchResults;
